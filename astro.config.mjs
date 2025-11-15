8f807e5d18bf45ad31da834f98a32c195fc757c2
jinseoit <jinseoit@gmail.com>
2025-11-15 09:56:02 +0900

    build : git hub 배포로직 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://jinseoit.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     rehypePlugins: [
